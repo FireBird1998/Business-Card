7ac6a14ca057771c9f8a3b5a5fc35d76c0936c02
Sawan Ghosh <sawan.ghosh37@gmail.com>
2022-12-31 11:52:38 +0530

    Basic structure of header and about page. CSS modification required

---
+++ src/components/Body.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="bodyContainer">
+<span>{{content}}</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Body_1',
+  props: {
+    content: {
+        default: ""
+    },
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="backgroundContainer">
+        <img src="../assets/tree-736885__480.jpg" class="backgroundImage">
+        <img src="../assets/profile_pic.webp" class="profilePic">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header_1'
+}
+</script>
+
+<style scoped>
+.backgroundImage {
+    height: auto;
+    width: 402px;
+}
+.backgroundContainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: top;
+    padding: 0px;
+    margin-left: -4px;
+    margin-top: -4px;
+}
+.profilePic {
+    height: 200px;
+    width: 200px;
+    align-self: center;
+    margin-top: -110px;
+    border-radius: 100px;
+}
+</style>
